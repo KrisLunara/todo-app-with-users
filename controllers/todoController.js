24b31a9af87975519b76a6d8ce1c46c03f45f4a6
9ut4 <9ut4@fresnou.org>
2024-02-28 09:26:13 -0800

    initial commit

---
+++ controllers/todoController.js
@@ -0,0 +1,93 @@
+const {Todo} = require('../models');
+
+
+module.exports.listAll = async function(req, res) {
+    const todos = await Todo.findAll({
+        where:{
+            user_id: req.user.id
+        }
+    });
+
+    let completeItems = todos.filter(item => item.complete);
+    let incompleteItems = todos.filter(item => !item.complete);
+
+    res.render('todos/viewAll', {
+        completeItems,
+        incompleteItems
+    });
+};
+
+
+module.exports.displayAddItem = function(req, res) {
+    const item = {
+        name: '',
+        description: '',
+    }
+    res.render('todos/newItem', {
+        item
+    })
+};
+
+module.exports.addNewItem = async function(req, res){
+    await Todo.create({
+        description: req.body.description,
+        user_id: req.user.id
+    });
+    res.redirect('/');
+};
+
+module.exports.viewEditItem = async function(req, res) {
+    const todo = await Todo.findOne({
+        where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }});
+    if (!todo){ //if we can't find it
+    res.redirect('/');
+    } else {
+        res.render('todos/editItem', {item: todo})
+    }
+};
+
+
+module.exports.saveEditItem = async function(req, res) {
+    await Todo.update({ description: req.body.description}, {
+        where:{
+            id: req.params.id,
+        }
+    })
+    res.redirect('/');
+};
+
+
+module.exports.deleteItem = async function(req, res) {
+    await Todo.destroy({
+        where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }
+    })
+    res.redirect('/');
+};
+
+
+module.exports.makeItemComplete = async function(req, res) {
+    await Todo.update({ complete:  true}, {
+        where:{
+            id: req.params.id,
+        }
+    })
+    res.redirect('/');
+};
+
+
+module.exports.markItemIncomplete = async function(req, res) {
+    await Todo.update({ complete:  false}, {
+        where:{
+            id: req.params.id,
+        }
+    })
+    res.redirect('/');
+};
+
+
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+const todoController = require('../controllers/todoController');
+const userController = require('../controllers/userController');
+
+function addUserToViews(req,res,next){
+    if (req.user){
+        res.locals.user = req.user;
+    }
+    next();
+}
+
+function redirectGuest(req,res,next){
+    if (!req.user){
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+/* GET home page. */
+router.get('/', addUserToViews, redirectGuest, todoController.listAll);
+
+router.get('/item/add', addUserToViews, todoController.displayAddItem);
+router.post('/item/add', addUserToViews, todoController.addNewItem);
+
+router.get('/item/edit/:id', addUserToViews, todoController.viewEditItem);
+router.post('/item/edit/:id', addUserToViews, todoController.saveEditItem);
+
+router.get('/item/delete/:id', addUserToViews, todoController.deleteItem);
+router.get('/item/complete/:id', addUserToViews, todoController.makeItemComplete);
+router.get('/item/incomplete/:id', addUserToViews, todoController.markItemIncomplete);
+
+router.get('/register', addUserToViews, userController.renderRegistration);
+router.post('/register', addUserToViews, userController.register);
+
+router.get('/login', addUserToViews, userController.renderLogin);
+router.post('/login', addUserToViews, userController.authenticate);
+
+router.get('/logout', addUserToViews, userController.logout);
+module.exports = router;
